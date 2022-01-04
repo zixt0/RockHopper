88f81ffbc3da786812deed1a77c3261bf723c53d
Philip Savage <psavage1995@gmail.com>
2022-01-04 15:25:03 +0100

    Added productcontroller and tests for it

--- data_access/ProductDB.js
+++ data_access/ProductDB.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 var fetch = require('node-fetch-commonjs');
 
 class ProductDB {
@@ -6,7 +5,13 @@ class ProductDB {
   //Get single product by id
   async getProductsById(id) {
     //Fetch from fakestore API, convert to json and return the data
-    var response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    var response = await fetch(`https://61d217e8da87830017e5940a.mockapi.io/api/rv/products/${id}`);
+    var data = await response.json();
+    return await data;
+  }
+
+  async getAllProducts() {
+    var response = await fetch(`https://61d217e8da87830017e5940a.mockapi.io/api/rv/products/`);
     var data = await response.json();
     return await data;
   }
---
+++ services/ProductController.js
@@ -0,0 +1,55 @@
+var ProductDB = require('../data_access/ProductDB');
+
+class ProductController {
+  constructor() {
+    this.prodDb = new ProductDB();
+  }
+  async getProductsWithVat() {
+    var products = await this.prodDb.getAllProducts();
+    return await this.calculateVatForProducts(products);
+  }
+
+  //Set vat for list of products
+  async calculateVatForProducts(products) {
+    //Convert products to array if not already an array
+    if(!Array.isArray(products)){
+      products = [products];
+    }
+    products.forEach(product => {
+      //80% of the profit of any sale is taxable
+      //VAT is 25% of the taxable profit
+      var profit = product.salesPrice - product.buyPrice;
+      var taxableProfit = profit * 0.8;
+      var vat = taxableProfit * 0.25;
+      //Reduce to 2 decimals
+      product.vat = Number(vat.toFixed(2));
+      //If VAT is negative, set to 0. Can't pay VAT on sale with no profit
+      if (product.vat < 0) {
+        product.vat = 0;
+      }
+
+
+    })
+    return products;
+  }
+
+  async getSumOfVat(products){
+    //If single product is provided outside of an array, return vat for that product
+    if(!Array.isArray(products)){
+      return products.vat;
+    } else {
+      var totalVat = 0;
+      products.forEach(product => {
+        totalVat += product.vat;
+      })
+      return totalVat;
+    }
+  }
+
+  //Create .xlsx file with product info, VAT
+  //Must also show how VAT was calculated
+  async exportProductsToExcel(products) {
+
+  }
+}
+module.exports = ProductController;
